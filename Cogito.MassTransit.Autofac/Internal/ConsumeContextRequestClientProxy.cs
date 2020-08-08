24ef9a77ff7221fae76b86ede3fdacbb695e4b1a
Jerome Haltom <jhaltom@alethic.solutions>
2020-08-08 10:48:48 -0500

    Build additional packages.

--- Cogito.MassTransit.Autofac/Internal/ConsumeContextRequestClientProxy.cs
+++ Cogito.MassTransit.Autofac/Internal/ConsumeContextRequestClientProxy.cs
@@ -27,34 +27,34 @@ namespace Cogito.MassTransit.Autofac.Internal
             this.impl = context.CreateRequestClient<TRequest>(context.GetPayload<IBus>());
         }
 
-        public RequestHandle<TRequest> Create(TRequest message, CancellationToken cancellationToken = default(CancellationToken), RequestTimeout timeout = default(RequestTimeout))
+        public RequestHandle<TRequest> Create(TRequest message, CancellationToken cancellationToken = default, RequestTimeout timeout = default)
         {
             return impl.Create(message, cancellationToken, timeout);
         }
 
-        public RequestHandle<TRequest> Create(object values, CancellationToken cancellationToken = default(CancellationToken), RequestTimeout timeout = default(RequestTimeout))
+        public RequestHandle<TRequest> Create(object values, CancellationToken cancellationToken = default, RequestTimeout timeout = default)
         {
             return impl.Create(values, cancellationToken, timeout);
         }
 
-        public Task<Response<T>> GetResponse<T>(TRequest message, CancellationToken cancellationToken = default(CancellationToken), RequestTimeout timeout = default(RequestTimeout)) where T : class
+        public Task<Response<T>> GetResponse<T>(TRequest message, CancellationToken cancellationToken = default, RequestTimeout timeout = default) where T : class
         {
             return impl.GetResponse<T>(message, cancellationToken, timeout);
         }
 
-        public Task<Response<T>> GetResponse<T>(object values, CancellationToken cancellationToken = default(CancellationToken), RequestTimeout timeout = default(RequestTimeout)) where T : class
+        public Task<Response<T>> GetResponse<T>(object values, CancellationToken cancellationToken = default, RequestTimeout timeout = default) where T : class
         {
             return impl.GetResponse<T>(values, cancellationToken, timeout);
         }
 
-        public Task<(Task<Response<T1>>, Task<Response<T2>>)> GetResponse<T1, T2>(TRequest message, CancellationToken cancellationToken = default(CancellationToken), RequestTimeout timeout = default(RequestTimeout))
+        public Task<(Task<Response<T1>>, Task<Response<T2>>)> GetResponse<T1, T2>(TRequest message, CancellationToken cancellationToken = default, RequestTimeout timeout = default)
             where T1 : class
             where T2 : class
         {
             return impl.GetResponse<T1, T2>(message, cancellationToken, timeout);
         }
 
-        public Task<(Task<Response<T1>>, Task<Response<T2>>)> GetResponse<T1, T2>(object values, CancellationToken cancellationToken = default(CancellationToken), RequestTimeout timeout = default(RequestTimeout))
+        public Task<(Task<Response<T1>>, Task<Response<T2>>)> GetResponse<T1, T2>(object values, CancellationToken cancellationToken = default, RequestTimeout timeout = default)
             where T1 : class
             where T2 : class
         {
