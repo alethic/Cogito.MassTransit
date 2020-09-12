4c00aec64b796c0f734b5b4c40c275684a80c506
Jerome Haltom <jhaltom@alethic.solutions>
2020-09-11 19:26:11 -0500

    Expose.

--- Cogito.MassTransit/ExceptionInfoException.cs
+++ Cogito.MassTransit/ExceptionInfoException.cs
@@ -6,7 +6,7 @@ namespace Cogito.MassTransit
 {
 
     /// <summary>
-    /// Encapsulates a <see cref="ExceptionInfo"/>.
+    /// Encapsulates a <see cref="global::MassTransit.ExceptionInfo"/>.
     /// </summary>
     public class ExceptionInfoException : Exception
     {
@@ -18,8 +18,13 @@ namespace Cogito.MassTransit
         public ExceptionInfoException(ExceptionInfo exceptionInfo) :
             base(exceptionInfo.Message, exceptionInfo.InnerException != null ? new ExceptionInfoException(exceptionInfo.InnerException) : null)
         {
-
+            ExceptionInfo = exceptionInfo;
         }
+
+        /// <summary>
+        /// Wrapped <see cref="global::MassTransit.ExceptionInfo"/> instance.
+        /// </summary>
+        public ExceptionInfo ExceptionInfo { get; }
 
     }
 
