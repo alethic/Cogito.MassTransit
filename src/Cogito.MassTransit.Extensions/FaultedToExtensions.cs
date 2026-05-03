10102c5b506eba4bf63148886a0ef4875919c935
Jerome Haltom <jhaltom@alethic.solutions>
2026-05-03 18:03:07 -0500

    Add ExceptionActivityBinder overloads to FaultedTo.

--- src/Cogito.MassTransit.Extensions/FaultedToExtensions.cs
+++ src/Cogito.MassTransit.Extensions/FaultedToExtensions.cs
@@ -88,6 +88,109 @@ namespace Cogito.MassTransit
             return source.Add(new FaultedToActivity<TSaga, TMessage, TRequest>(context => Task.FromResult(requestTokenFactory(context)), context => Task.FromResult(exception), contextCallback));
         }
 
+        /// <summary>
+        /// Responds to a stored request with a fault from inside a <c>Catch</c> handler.
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TException"></typeparam>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="requestTokenFactory"></param>
+        /// <param name="exceptionFactory"></param>
+        /// <param name="contextCallback"></param>
+        /// <returns></returns>
+        public static ExceptionActivityBinder<TSaga, TMessage, TException> FaultedToAsync<TSaga, TMessage, TException, TRequest>(this ExceptionActivityBinder<TSaga, TMessage, TException> source, AsyncRequestTokenFactory<TSaga, TMessage, TRequest> requestTokenFactory, AsyncExceptionFactory<TSaga, TMessage, TRequest> exceptionFactory, Action<SendContext<FaultEvent<TRequest>>>? contextCallback = null)
+            where TSaga : class, SagaStateMachineInstance
+            where TMessage : class
+            where TException : Exception
+            where TRequest : class
+        {
+            return source.Add(new FaultedToActivity<TSaga, TMessage, TRequest>(requestTokenFactory, exceptionFactory, contextCallback));
+        }
+
+        /// <summary>
+        /// Responds to a stored request with a fault from inside a <c>Catch</c> handler, using the caught exception.
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TException"></typeparam>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="requestTokenFactory"></param>
+        /// <param name="contextCallback"></param>
+        /// <returns></returns>
+        public static ExceptionActivityBinder<TSaga, TMessage, TException> FaultedToAsync<TSaga, TMessage, TException, TRequest>(this ExceptionActivityBinder<TSaga, TMessage, TException> source, AsyncRequestTokenFactory<TSaga, TMessage, TRequest> requestTokenFactory, Action<SendContext<FaultEvent<TRequest>>>? contextCallback = null)
+            where TSaga : class, SagaStateMachineInstance
+            where TMessage : class
+            where TException : Exception
+            where TRequest : class
+        {
+            return source.Add(new FaultedToActivity<TSaga, TMessage, TRequest>(requestTokenFactory, context => Task.FromResult((Exception)((BehaviorExceptionContext<TSaga, TMessage, TException>)context).Exception), contextCallback));
+        }
+
+        /// <summary>
+        /// Responds to a stored request with a fault from inside a <c>Catch</c> handler.
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TException"></typeparam>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="requestTokenFactory"></param>
+        /// <param name="exceptionFactory"></param>
+        /// <param name="contextCallback"></param>
+        /// <returns></returns>
+        public static ExceptionActivityBinder<TSaga, TMessage, TException> FaultedTo<TSaga, TMessage, TException, TRequest>(this ExceptionActivityBinder<TSaga, TMessage, TException> source, RequestTokenFactory<TSaga, TMessage, TRequest> requestTokenFactory, ExceptionFactory<TSaga, TMessage, TRequest> exceptionFactory, Action<SendContext<FaultEvent<TRequest>>>? contextCallback = null)
+            where TSaga : class, SagaStateMachineInstance
+            where TMessage : class
+            where TException : Exception
+            where TRequest : class
+        {
+            return source.Add(new FaultedToActivity<TSaga, TMessage, TRequest>(context => Task.FromResult(requestTokenFactory(context)), context => Task.FromResult(exceptionFactory(context)), contextCallback));
+        }
+
+        /// <summary>
+        /// Responds to a stored request with a fault from inside a <c>Catch</c> handler, using the caught exception.
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TException"></typeparam>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="requestTokenFactory"></param>
+        /// <param name="contextCallback"></param>
+        /// <returns></returns>
+        public static ExceptionActivityBinder<TSaga, TMessage, TException> FaultedTo<TSaga, TMessage, TException, TRequest>(this ExceptionActivityBinder<TSaga, TMessage, TException> source, RequestTokenFactory<TSaga, TMessage, TRequest> requestTokenFactory, Action<SendContext<FaultEvent<TRequest>>>? contextCallback = null)
+            where TSaga : class, SagaStateMachineInstance
+            where TMessage : class
+            where TException : Exception
+            where TRequest : class
+        {
+            return source.Add(new FaultedToActivity<TSaga, TMessage, TRequest>(context => Task.FromResult(requestTokenFactory(context)), context => Task.FromResult((Exception)((BehaviorExceptionContext<TSaga, TMessage, TException>)context).Exception), contextCallback));
+        }
+
+        /// <summary>
+        /// Responds to a stored request with a fault from inside a <c>Catch</c> handler.
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TException"></typeparam>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="requestTokenFactory"></param>
+        /// <param name="exception"></param>
+        /// <param name="contextCallback"></param>
+        /// <returns></returns>
+        public static ExceptionActivityBinder<TSaga, TMessage, TException> FaultedTo<TSaga, TMessage, TException, TRequest>(this ExceptionActivityBinder<TSaga, TMessage, TException> source, RequestTokenFactory<TSaga, TMessage, TRequest> requestTokenFactory, Exception exception, Action<SendContext<FaultEvent<TRequest>>>? contextCallback = null)
+            where TSaga : class, SagaStateMachineInstance
+            where TMessage : class
+            where TException : Exception
+            where TRequest : class
+        {
+            return source.Add(new FaultedToActivity<TSaga, TMessage, TRequest>(context => Task.FromResult(requestTokenFactory(context)), context => Task.FromResult(exception), contextCallback));
+        }
+
     }
 
 }
